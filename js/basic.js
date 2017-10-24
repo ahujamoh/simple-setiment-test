605b1c3419bbf8a69eb9e3de511329bcd51a9e03
Mohit Ahuja <ahujamoh@gmail.com>
2017-10-24 11:32:19 +0200

    submit->button

--- js/basic.js
+++ js/basic.js
@@ -1,3 +1,13 @@
-function myFunction(){
+function myFunction() {
     console.log("tweet pressed");
+    var tweet = document.getElementById("tweetContent").value;
+    data = {"tweet": tweet};
+    console.log("You wrote " + tweet);
+    var URL = 'http://54.213.252.131/sentiment';
+    $.post(URL, data, function (response, status, xhr) {
+        console.log(response);
+        console.log(status);
+        console.log(xhr);
+        alert(response.sentiment);
+    });
 }
